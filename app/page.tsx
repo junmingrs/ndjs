bada28ba30fc89ff3a6ce1b7300cac8cf5127277
junmingrs <tjunming29@gmail.com>
2026-04-22 18:35:40 +0800

    added supabase google provider support

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 import Calendar from "@/components/Calendar";
+import { LoginForm } from "@/components/Login";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Page() {
-  return <Calendar />;
+export default async function Page() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return (
+    <>
+      <Calendar />
+      {/* do popup here */}
+      {!user ? <LoginForm /> : null}
+    </>
+  );
 }
